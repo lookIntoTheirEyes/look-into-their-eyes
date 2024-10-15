176e54c6878d34d4b0fb5207b465cec43a864d2c
Nati Gurevich <nati_g4@hotmail.com>
2024-10-15 20:03:02 +0300

    Added about and footer

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,7 +1,12 @@
-// import { useTranslations } from "next-intl";
-// import styles from "./page.module.css";
+import { useTranslations } from "next-intl";
+import styles from "./page.module.css";
 
 export default function About() {
-  //   const t = useTranslations("HomePage");
-  return <p>this is the about page</p>;
+  const t = useTranslations("About");
+  return (
+    <div className={styles.container}>
+      <h1>{t("intro")}</h1>
+      <p> {t("artist")}</p>
+    </div>
+  );
 }
