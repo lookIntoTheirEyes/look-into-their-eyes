0db296c93d89fe1c9326788629e2e9cd4adc4683
Nati Gurevich <nati_g4@hotmail.com>
2024-11-17 13:19:44 +0200

    Added metadata to story page

--- src/app/[locale]/story/page.tsx
+++ src/app/[locale]/story/page.tsx
@@ -1,16 +1,41 @@
-import { Language } from "@/lib/model/language";
+import { ILanguageProps } from "@/lib/model/language";
 import { getTranslations } from "next-intl/server";
 import BookContainer from "@/components/Book/BookContainer";
+import { getPageNum, SearchParams } from "@/lib/utils/utils";
+import { getHero } from "@/lib/utils/heroesService";
 
-interface Props {
-  params: Promise<Params>;
+interface IProps extends ILanguageProps {
+  searchParams: Promise<SearchParams>;
 }
 
-type Params = {
-  locale: Language;
-};
+export async function generateMetadata(props: IProps) {
+  const searchParams = await props.searchParams;
+  const params = await props.params;
 
-const BookComponent: React.FC<Props> = async (props) => {
+  const { locale } = params;
+  const pageNum = getPageNum(searchParams);
+  try {
+    const { name: title, description } = await getHero(pageNum, locale);
+
+    return {
+      title,
+      description,
+    };
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    const t = await getTranslations("Metadata");
+
+    const title = t("layout.title");
+    const description = t("layout.description");
+
+    return {
+      title,
+      description,
+    };
+  }
+}
+
+const BookComponent: React.FC<IProps> = async (props) => {
   const { locale } = await props.params;
   const t = await getTranslations("Story");
 
