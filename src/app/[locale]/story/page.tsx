235196a12fab2df631c777e6b280cc82bf5b3f86
Nati Gurevich <nati_g4@hotmail.com>
2024-11-18 11:07:51 +0200

    Refactor to gain more control

--- src/app/[locale]/story/page.tsx
+++ src/app/[locale]/story/page.tsx
@@ -1,8 +1,18 @@
-import { ILanguageProps } from "@/lib/model/language";
+import { ILanguageProps, Language } from "@/lib/model/language";
 import { getTranslations } from "next-intl/server";
 import BookContainer from "@/components/Book/BookContainer";
 import { getPageNum, SearchParams } from "@/lib/utils/utils";
-import { getHero } from "@/lib/utils/heroesService";
+import {
+  getAllPages,
+  getBackPage,
+  getFrontPage,
+  getHero,
+  NO_CONTENT_PAGES_TOC,
+  PAGES_FACTOR_TOC,
+} from "@/lib/utils/heroesService";
+import Page from "@/components/Book/Page/Page";
+import PageContent from "@/components/Book/Page/PageContent/PageContent";
+import PageCover from "@/components/Book/PageCover/PageCover";
 
 interface IProps extends ILanguageProps {
   searchParams: Promise<SearchParams>;
@@ -39,16 +49,40 @@ const BookComponent: React.FC<IProps> = async (props) => {
   const { locale } = await props.params;
   const t = await getTranslations("Story");
 
+  const bookPages = getAllPages(locale);
+  const rtl = locale === Language.he;
+
+  const pagesAmount = bookPages.length + NO_CONTENT_PAGES_TOC;
+
+  const pageNum = (i: number) =>
+    (rtl ? pagesAmount - i - NO_CONTENT_PAGES_TOC : i + 1) + PAGES_FACTOR_TOC;
+
+  const Pages = structuredClone(bookPages).map((content, i) => (
+    <Page rtl={rtl} key={content.title} pageNum={pageNum(i)}>
+      <PageContent
+        cta={t("actions.pageCta")}
+        details={content}
+        pageNum={pageNum(i)}
+        title={t("title")}
+      />
+    </Page>
+  ));
+
+  const Front = <PageCover details={getFrontPage(locale)} />;
+  const Back = <PageCover details={getBackPage(locale)} />;
+
   return (
     <BookContainer
-      locale={locale}
+      rtl={rtl}
       text={{
         tableOfContentsTitle: t("tableOfContents"),
-        pageCta: t("actions.pageCta"),
-        pageTitle: t("title"),
         next: t("actions.next"),
         previous: t("actions.previous"),
       }}
+      Pages={Pages}
+      Front={Front}
+      Back={Back}
+      pagesContent={bookPages}
     />
   );
 };
--- src/components/Book/BookContainer.tsx
+++ src/components/Book/BookContainer.tsx
@@ -1,64 +1,32 @@
-import { Language } from "@/lib/model/language";
-import {
-  getAllPages,
-  getBackPage,
-  getFrontPage,
-  NO_CONTENT_PAGES,
-  NO_CONTENT_PAGES_TOC,
-  PAGES_FACTOR,
-  PAGES_FACTOR_TOC,
-} from "@/lib/utils/heroesService";
-import PageCover from "./PageCover/PageCover";
-import Page from "./Page/Page";
-import PageContent from "./Page/PageContent/PageContent";
+import { Page as BookPage } from "@/lib/utils/heroesService";
 import PageContainer from "../PageContainer/PageContainer";
 import Book from "./Book";
 
 interface IBookProps {
-  locale: Language;
+  rtl: boolean;
   text: {
     tableOfContentsTitle: string;
-    pageCta: string;
-    pageTitle: string;
     next: string;
     previous: string;
   };
+  Pages: JSX.Element[];
+  Front: JSX.Element;
+  Back: JSX.Element;
+  pagesContent: BookPage[];
 }
 
 const BookContainer: React.FC<IBookProps> = ({
-  locale,
-  text: { tableOfContentsTitle, pageCta, pageTitle, next, previous },
+  rtl,
+  text: { tableOfContentsTitle, next, previous },
+  Pages,
+  pagesContent,
+  Front,
+  Back,
 }) => {
-  const bookPages = getAllPages(locale);
-  const noContentPages = tableOfContentsTitle
-    ? NO_CONTENT_PAGES_TOC
-    : NO_CONTENT_PAGES;
-  const pagesFactor = tableOfContentsTitle ? PAGES_FACTOR_TOC : PAGES_FACTOR;
-
-  const pagesAmount = bookPages.length + noContentPages;
-
-  const rtl = locale === Language.he;
-
   const toc = {
     title: tableOfContentsTitle,
-    pages: structuredClone(bookPages),
+    pages: pagesContent,
   };
-
-  const pageNum = (i: number) =>
-    (rtl ? pagesAmount - i - noContentPages : i + 1) + pagesFactor;
-
-  const Front = <PageCover details={getFrontPage(locale)} />;
-  const Back = <PageCover details={getBackPage(locale)} />;
-  const Pages = bookPages.map((content, i) => (
-    <Page rtl={rtl} key={content.title} pageNum={pageNum(i)}>
-      <PageContent
-        cta={pageCta}
-        details={content}
-        pageNum={pageNum(i)}
-        title={pageTitle}
-      />
-    </Page>
-  ));
 
   return (
     <PageContainer isStory>
