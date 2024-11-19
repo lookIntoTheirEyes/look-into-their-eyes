41e4d33edd793e02b8fc439f39d7e4d2169e34fc
Nati Gurevich <nati_g4@hotmail.com>
2024-11-19 16:36:09 +0200

    Simplify routing page

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,27 +2,29 @@ import { createNavigation } from "next-intl/navigation";
 import { defineRouting } from "next-intl/routing";
 import { Language } from "@/lib/model/language";
 
-export const pages = [
-  "/",
-  "/about",
-  "/story",
-  "/story/details",
-  "/privacy",
-  "/terms",
-  "/accessibility",
-  "/families",
-  "/families/new",
-  "/visitors-book",
-  "/visitors-book/new",
-];
+const pathnames = {
+  "/": "/",
+  "/about": "/about",
+  "/story": "/story",
+  "/story/details": "/story/details",
+  "/accessibility": "/accessibility",
+  "/privacy": "/privacy",
+  "/terms": "/terms",
+  "/visitors-book": "/visitors-book",
+  "/visitors-book/new": "/visitors-book/new",
+  "/families": "/families",
+  "/families/new": "/families/new",
+} as const;
+
+export const pages = Object.keys(pathnames) as (keyof typeof pathnames)[];
 
 export const routing = defineRouting({
   locales: Object.values(Language),
   defaultLocale: Language.he,
-  pathnames: Object.fromEntries(pages.map((page) => [page, page])),
+  pathnames,
 });
 
-export type Pathnames = keyof typeof routing.pathnames;
+export type Pathnames = keyof typeof pathnames;
 export type Locale = (typeof routing.locales)[number];
 
 export const { Link, getPathname, redirect, usePathname, useRouter } =
