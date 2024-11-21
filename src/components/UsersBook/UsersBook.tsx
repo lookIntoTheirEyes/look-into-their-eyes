91d0d1ee53a23bcb6ae892d561153d1194959f55
Nati Gurevich <nati_g4@hotmail.com>
2024-11-21 20:08:41 +0200

    Added more logic to adding a comment

--- src/components/UsersBook/UsersBook.tsx
+++ src/components/UsersBook/UsersBook.tsx
@@ -1,8 +1,9 @@
+import { Pathnames } from "@/i18n/routing";
 import BookContainer from "@/components/Book/BookContainer";
 import PageCover from "@/components/Book/PageCover/PageCover";
 import { Page as BookPage } from "@/lib/model/book";
-import NewCommentButton from "./NewCommentButton/NewCommentButton";
-import { Pathnames } from "@/i18n/routing";
+import NewCommentButton from "@/components/UsersBook/NewCommentButton/NewCommentButton";
+import styles from "./UsersBook.module.css";
 
 interface IProps {
   title: string;
@@ -22,6 +23,24 @@ const UsersBook: React.FC<IProps> = ({ title, rtl, newText, newPath }) => {
   const Pages = [] as JSX.Element[];
   const pagesContent = [] as BookPage[];
 
+  const newButton = (
+    <NewCommentButton
+      pathname={newPath}
+      text={newText}
+      pad={!!pagesContent.length}
+    />
+  );
+
+  if (!pagesContent.length) {
+    const msg = newPath === "/visitors/new" ? "commented" : "shared";
+    return (
+      <div className={styles.noComment}>
+        <h1>No one has {msg} yet, click here button to be the first one! </h1>
+        {newButton}
+      </div>
+    );
+  }
+
   return (
     <BookContainer
       pagesContent={pagesContent}
@@ -31,7 +50,7 @@ const UsersBook: React.FC<IProps> = ({ title, rtl, newText, newPath }) => {
       Back={Back}
       noContentAmount={1}
     >
-      <NewCommentButton pathname={newPath} text={newText} />
+      {newButton}
     </BookContainer>
   );
 };
