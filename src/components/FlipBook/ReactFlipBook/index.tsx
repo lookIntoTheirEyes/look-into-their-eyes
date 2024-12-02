5def8a0ba3f3b0d64a61d1b4e9cf59b1f59d6944
Nati Gurevich <nati_g4@hotmail.com>
2024-12-02 20:07:14 +0200

    Fix view

--- src/components/FlipBook/ReactFlipBook/index.tsx
+++ src/components/FlipBook/ReactFlipBook/index.tsx
@@ -65,7 +65,7 @@ const HTMLFlipBookForward = React.forwardRef<
       }
     }
   }, [
-    pages.length,
+    // pages.length,
     props.children,
     props.renderOnlyPageLengthChange,
     props.rtl,
