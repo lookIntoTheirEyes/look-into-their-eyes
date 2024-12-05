0e054664a927674514fb8f1f6c1d144d8a5f55cc
Nati Gurevich <nati_g4@hotmail.com>
2024-12-05 15:55:27 +0200

    added direction

--- src/components/Book/NewBook/hooks/usePageFlip.ts
+++ src/components/Book/NewBook/hooks/usePageFlip.ts
@@ -163,6 +163,7 @@ export const usePageFlip = ({
               ...from(),
               immediate: true,
               progress,
+              direction: memo.direction,
             };
           });
         }
